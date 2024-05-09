a6b8f4cd6e04e32166db33ac5d107bf3f0c92fa9
David Benjamin <davidnbenjamin15@gmail.com>
2024-05-09 16:34:22 -0400

    Add waiting animation to terminal.

--- schedule_task.py
+++ schedule_task.py
@@ -1,18 +1,25 @@
 import schedule
+import sys
 import time
 import subprocess
 
 def run_daily_task():
-    subprocess.Popen(['python', 'main.py'])
-    time.sleep(30)
-    subprocess.Popen(['python', 'main.py'])
-    time.sleep(30)
+    global task_executed
     subprocess.Popen(['python', 'main.py'])
     print("Task running")
+    task_executed = True
 
 schedule.every().day.at("23:59:57").do(run_daily_task)
 print("Running schedule_task.py")
 
+animation = ['', '.', '..', '...']
 while True:
-    schedule.run_pending()
-    time.sleep(1) # check every ... seconds
+    for char in animation:
+        schedule.run_pending() # check if it's time to run the task. All other code in this loop is for aesthetics.
+        if (task_executed):
+            break
+        sys.stdout.write('\rWaiting' + char)
+        sys.stdout.flush()
+        time.sleep(0.5)
+    sys.stdout.write('\r' + ' ' * 15)  # Add spaces to clear the ellipses
+    sys.stdout.flush()
